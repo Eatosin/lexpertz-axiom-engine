eb7077f2b6083a619232a131d63cbc27068c9abb
Owadokun Tosin Tobi <89821134+Eatosin@users.noreply.github.com>
2026-01-31 01:55:56 +0100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          cyan: "#06b6d4",
+          blue: "#2563eb",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
