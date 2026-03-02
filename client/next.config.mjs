09c760fa1da5cb7b378fe700e377a1a55dd26071
Owadokun Tosin Tobi <89821134+Eatosin@users.noreply.github.com>
2026-03-02 11:11:25 +0100

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // This stops the "Internal Error" during the deployment output phase.
+  serverExternalPackages: ['@react-pdf/renderer'],
+  
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', '*.vercel.app']
