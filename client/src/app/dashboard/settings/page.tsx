c3b79fd87dc8ca614d44b31a647e77bd832570d8
Owadokun Tosin Tobi <tosinowadokun11@gmail.com>
2026-04-20 10:35:40 +0100

    chore: scorched earth - purged legacy dashboard logic for V4.6 rebuild

--- client/src/app/dashboard/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ApiKeysManager } from "@/components/api-keys-manager";
-
-export default function SettingsPage() {
-  return (
-    <div className="flex-1 overflow-y-auto p-6 md:p-12 bg-background custom-scrollbar">
-      <ApiKeysManager />
-    </div>
-  );
-}
